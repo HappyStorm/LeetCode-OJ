d2da80043a01a5957995646a889188a04dcc1b15
HappyStorm <gogogo753951741963@gmail.com>
2020-04-12 17:20:32 +0800

    [Add] add #1046 naive solution

---
+++ 1046. Last Stone Weight/1046_naive.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        while len(stones) > 1:
+            stones = sorted(stones)
+            if stones[-1] == stones[-2]:
+                stones.pop()
+                stones.pop()
+
+            else:
+                stones[-2] = stones[-1] - stones[-2]
+                stones.pop()
+
+        return stones[0] if len(stones) else 0
