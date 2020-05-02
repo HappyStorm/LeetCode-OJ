676854eda418d91d4271816b491999b105baca2e
HappyStorm <gogogo753951741963@gmail.com>
2020-05-02 22:24:28 +0800

    [Add] add #771

---
+++ 0771. Jewels and Stones/771.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        return len([c for c in S if c in J])
