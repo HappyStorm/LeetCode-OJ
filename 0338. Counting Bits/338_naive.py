596e2ee02a55bb5a5f349caf885d526387123113
HappyStorm <gogogo753951741963@gmail.com>
2020-05-28 19:14:55 +0800

    [Add] add #338 naive solution

---
+++ 0338. Counting Bits/338_naive.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        ans = [0 for _ in range(num+1)]
+        for i in range(1, num+1):
+            ans[i] = ans[i>>1] + (i%2)
+
+        return ans
