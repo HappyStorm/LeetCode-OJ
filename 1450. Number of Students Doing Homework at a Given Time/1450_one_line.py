27fdac1415281282a209d8fbcb45502db3772a51
HappyStorm <gogogo753951741963@gmail.com>
2020-10-22 20:22:51 +0800

    [Add] add #1450 simple one line

---
+++ 1450. Number of Students Doing Homework at a Given Time/1450_one_line.py
@@ -0,0 +1,3 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        return sum([1 for s, e in zip(startTime, endTime) if s <= queryTime <= e])
