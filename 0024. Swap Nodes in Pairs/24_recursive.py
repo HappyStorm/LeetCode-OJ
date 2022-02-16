43ecc1c8613a1e76c7ac03f68f6b7d13f90cf009
HappyStorm <gogogo753951741963@gmail.com>
2022-02-16 23:53:06 +0800

    [Add] add #0024 recursive ver.

---
+++ 0024. Swap Nodes in Pairs/24_recursive.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        tmp = head.next
+        head.next = self.swapPairs(tmp.next)
+        tmp.next = head
+        return tmp
